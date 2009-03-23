cd80bbb34c2f9cc880516e132144742e1fa3efa6
wangguoqin1001 <wangguoqin1001@5ffd2ba2-e74d-0410-be6f-8996228c200c>
2009-03-23 09:14:17 +0000

    Jinan Oct 28th after match

--- Paint.c
+++ Paint.c
@@ -5,23 +5,24 @@
 #include "Paint.h"
 #include "BottomLayer.h"
 
-inline int PrintColor(unsigned char *frame, int c, int color)
+int PrintColor(unsigned char *frame, int c, int color)
 {
-	if (c >= 0 && c <= CAPTURE_WIDTH * CAPTURE_HEIGHT - 1) {
+	if (c >= 0 && c <= CAPTURE_WIDTH * CAPTURE_HEIGHT - 1)
+	{
 		frame[c * 3] = COLOR_VALUE_RGB[color][0];
 		frame[c * 3 + 1] = COLOR_VALUE_RGB[color][1];
 		frame[c * 3 + 2] = COLOR_VALUE_RGB[color][2];
 	}
-
 	return 1;
 }
 
-inline int DrawBigPoint(unsigned char *frame, int Center, int Radius, int color)
+int DrawBigPoint(unsigned char *frame, int Center, int Radius, int color)
 {
 	int i, j;
 
 	for (i = 0; i < Radius; i++)
-		for (j = 0; j < Radius; j++) {
+		for (j = 0; j < Radius; j++)
+		{
 			PrintColor(frame, Center + i, color);
 			PrintColor(frame, Center - i, color);
 			PrintColor(frame, Center + CAPTURE_WIDTH * i + j, color);
